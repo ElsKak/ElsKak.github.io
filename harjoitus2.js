8ff2f30d5f7fc044ad86ca1c0ac73fcc07b2faca
ElsKak <kakkurieelis2004@gmail.com>
2020-08-19 14:30:45 +0300

    harjoitus2 19.8

---
+++ harjoitus2.js
@@ -0,0 +1,67 @@
+console.log("Kohta A")
+//KOHTA A
+let eka_luku = -43;
+let toka_luku = 55;
+
+if (eka_luku > toka_luku) {
+    console.log("eka_luku on isompi")
+}
+else (eka_luku < toka_luku); {
+    console.log("toka_luku on isompi")
+}
+
+//KOHTA B
+console.log("Kohta B")
+if (eka_luku = toka_luku) {
+    console.log("luvut ovat samat")
+}
+
+//KOHTA C a)
+console.log("Kohta C a)")
+let luku1 = -34;
+let luku2 = 55;
+let luku3 = 21;
+
+if (luku1 > luku2 && luku1 > luku3) {
+    console.log("luku1 on isoin");
+} else if (luku1 < luku2 || luku2 > luku3) {
+    console.log("luku2 on isoin");
+}
+
+//KOHTA C b)
+console.log("Kohta C b)")
+let luku_yksi = -3;
+let luku_kaksi = -56;
+let luku_kolme = -54;
+
+if (luku_yksi > luku_kaksi && luku_yksi > luku_kolme) {
+    console.log("luku_yksi on isoin");
+} else if (luku_yksi < luku_kaksi || luku_kaksi > luku_kolme) {
+    console.log("luku_kaksi on isoin");
+}
+
+//KOHTA D a)
+console.log("Kohta D a)")
+let tuotto1 = 56000;
+let kulut1 = 61000;
+let tulos1 = tuotto1 - kulut1;
+
+if (tuotto1 > kulut1) {
+    console.log("voittoa");
+}
+else if (tuotto1 < kulut1) {
+    console.log("tappiota");
+}
+
+//KOHTA D b)
+console.log("Kohta D b)")
+let tuotto2 = 56000;
+let kulut2 = 5000;
+let tulos2 = tuotto2 - kulut2;
+
+if (tuotto2 > kulut2) {
+    console.log("voittoa");
+}
+else if (tuotto2 < kulut2) {
+    console.log("tappiota");
+}
